063e64b79abcd698a2b337c1bafe702cd332cabb
ipaully <ipaully22@gmail.com>
2018-09-30 21:55:23 +0300

    [FEATURE] Add student object class

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
-from flask import Flask
+from flask import Flask, request, redirect, url_for, render_template
+from student import Student
 
 app = Flask(__name__)
+
+students = []
 
 if __name__ == '__main__':
     app.run(debug=True)
