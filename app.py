57806b3686f59996dca6b07d86d40ee6032b56a9
ipaully <ipaully22@gmail.com>
2018-09-30 22:58:40 +0300

    [FEAT] add endpoint to retrieve one student info

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ app = Flask(__name__)
 
 students = []
 
+def find_student(student_id):
+    return [student for student in students if student.id == student_id][0]
+
 @app.route('/')
 def root():
     return redirect(url_for('index'))
@@ -20,5 +23,11 @@ def index():
 @app.route('/students/new')
 def new():
     return render_template('new.html')
+
+@app.route('/students/<int:id>')
+def show(id):
+    found_student = find_student(id)
+    return render_template('show.html', student=found_student)
+
 if __name__ == '__main__':
     app.run(debug=True)
