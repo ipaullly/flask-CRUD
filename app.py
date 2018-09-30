8f3471e601be71620d09a9f9755387c920f62674
ipaully <ipaully22@gmail.com>
2018-09-30 22:12:09 +0300

    [FEAT] Add GET students endpoint

--- app.py
+++ app.py
@@ -5,5 +5,12 @@ app = Flask(__name__)
 
 students = []
 
+@app.route('/')
+def root():
+    return redirect(url_for('index'))
+
+@app.route('/students', methods=["GET", "POST"])
+def index():
+    return render_template('index.html', students=students)
 if __name__ == '__main__':
     app.run(debug=True)
